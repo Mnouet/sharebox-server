079e73e483e0199aa3c2309737a707465a3c3720
Alexis BRIARD <briard.alexis@hotmail.fr>
2016-06-15 13:56:21 +0200

    stash

--- src/main/java/com/japrod/sharebox/server/configuration/oauth/AuthorizationServerConfiguration.java
+++ src/main/java/com/japrod/sharebox/server/configuration/oauth/AuthorizationServerConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
@@ -36,6 +37,12 @@ public class AuthorizationServerConfiguration extends AuthorizationServerConfigu
 
 	@Autowired
 	private DataSource dataSource;
+	
+	@Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) 
+      throws Exception {
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
